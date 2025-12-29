8e7276712a9504ad020595496598a428baab52ac
himanshusingh1312 <himanshu094405@gmail.com>
2025-12-29 23:38:47 +0530

    add google analytics in layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 import AssistantWidget from "./components/AssistantWidget";
@@ -12,19 +13,35 @@ const outfit = Outfit({
 export const metadata: Metadata = {
   title: "Crazy Cakes",
   description: "Welcome to Crazy Cakes - Cakes & Pastries",
-icons: {
-  icon: [{ url: "/cakelogo.png", sizes: "32x32", type: "image/png" }],
-  apple: [{ url: "/cakelogo.png", sizes: "180x180" }],
-}
-}
+  icons: {
+    icon: [{ url: "/cakelogo.png", sizes: "32x32", type: "image/png" }],
+    apple: [{ url: "/cakelogo.png", sizes: "180x180" }],
+  },
+};
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+          `}
+        </Script>
+      </head>
+
       <body className={`${outfit.className} antialiased`}>
         {children}
 
@@ -34,4 +51,3 @@ export default function RootLayout({
     </html>
   );
 }
-
